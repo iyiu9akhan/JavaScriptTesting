6f15ee47c05a50675c758cabb2406ea2b2df8d09
iyiu9akhan <iyiu9akhan@gmail.com>
2025-03-15 04:02:42 +0600

    result page structure design || return home button fixed || div disable / enable with click

--- bmi calculator/js/script.js
+++ bmi calculator/js/script.js
@@ -3,15 +3,17 @@ let error_sign1 = document.getElementsByClassName("error_sign")[1];
 let error_sign2 = document.getElementsByClassName("error_sign")[2];
 let error_sign3 = document.getElementsByClassName("error_sign")[3];
 let error_sign4 = document.getElementsByClassName("error_sign")[4];
-
-// let error_sign1 = document.querySelector(".error_sign1");
-// let error_sign2 = document.querySelector(".error_sign2");
-// let error_sign3 = document.querySelector(".error_sign3");
-// let error_sign4 = document.querySelector(".error_sign4");
-// let error_sign5 = document.querySelector(".error_sign5");
-
-
 let error_text = document.querySelector(".error_text");
+let form_top = document.querySelector(".top");
+let form_middle = document.querySelector(".middle");
+let form_bottom= document.querySelector(".bottom");
+let result_middle = document.querySelector(".result_portion");
+let result_bottom = document.querySelector(".result_bottom")
+
+result_middle.style.display = "none";
+result_bottom.style.display = "none";
+
+
 const calculate = () => {
   const name = document.getElementById("name").value;
   const gender = document.getElementById("gender").value;
@@ -20,61 +22,57 @@ const calculate = () => {
   const weight = document.getElementById("weight").value;
 
   if (!isNaN(name)) {
-    // error_sign1.textContent = "!";
-    // error_sign2.textContent = "";
-    // error_sign3.textContent = "";
-    // error_sign4.textContent = "";
-    // error_sign5.textContent = "";
-
-    error_sign0.innerHTML = "1";
+    error_sign0.innerHTML = "!";
+    error_sign1.innerHTML = "";
+    error_sign2.innerHTML = "";
+    error_sign3.innerHTM = "";
+    error_sign4.innerHTML = "";
     error_text.textContent = "missing name";
   } else if (!isNaN(gender)) {
-    // error_sign1.textContent = "";
-    // error_sign2.textContent = "!";
-    // error_sign3.textContent = "";
-    // error_sign4.textContent = "";
-    // error_sign5.textContent = "";
-    error_sign0.innerHTML = "";
-
-    error_sign1.innerHTML = "2";
+    error_sign0.innerHTM = "";
+    error_sign1.innerHTML = "!";
+    error_sign2.innerHTM = "";
+    error_sign3.innerHTM = "";
+    error_sign4.innerHTML = "";
     error_text.textContent = "missing gender";
   } else if (!isNaN(age)) {
-    // error_sign1.textContent = "";
-    // error_sign2.textContent = "";
-    // error_sign3.textContent = "!";
-    // error_sign4.textContent = "";
-    // error_sign5.textContent = "";
+    error_sign0.innerHTML = "";
     error_sign1.innerHTML = "";
-    error_sign2.innerHTML = "3";
+    error_sign2.innerHTML = "!";
+    error_sign3.innerHTML = "";
+    error_sign4.innerHTML = "";
     error_text.textContent = "missing age";
   } else if (!isNaN(height)) {
-    // error_sign1.textContent = "";
-    // error_sign2.textContent = "";
-    // error_sign3.textContent = "";
-    // error_sign4.textContent = "!";
-    // error_sign5.textContent = "";
+    error_sign0.innerHTML = "";
+    error_sign1.innerHTML = "";
     error_sign2.innerHTML = "";
-
-    error_sign3.innerHTML = "4";
+    error_sign3.innerHTML = "!";
+    error_sign4.innerHTML = "";
     error_text.textContent = "missing height";
   } else if (!isNaN(weight)) {
-    // error_sign1.textContent = "";
-    // error_sign2.textContent = "";
-    // error_sign3.textContent = "";
-    // error_sign4.textContent = "";
-    // error_sign5.textContent = "!";
+    error_sign0.innerHTML = "";
+    error_sign1.innerHTML = "";
+    error_sign2.innerHTML = "";
     error_sign3.innerHTML = "";
-
-    error_sign4.innerHTML = "5";
+    error_sign4.innerHTML = "!";
     error_text.textContent = "missing weight";
   } else {
-    // error_sign1.textContent = "";
-    // error_sign2.textContent = "";
-    // error_sign3.textContent = "";
-    // error_sign4.textContent = "";
-    // error_sign5.textContent = "";
-    error_sign4.innerHTML = "";
+    form_top.style.display = "none";
+    form_middle.style.display = "none";
+    form_bottom.style.display = "none";
 
-    error_text.textContent = "done";
+    result_middle.style.display = "block";
+    result_bottom.style.display = "block";
+    error_sign0.innerHTML = "";
+    error_sign1.innerHTML = "";
+    error_sign2.innerHTML = "";
+    error_sign3.innerHTML = "";
+    error_sign4.innerHTML = "";
+    error_text.textContent = "";
   }
 };
+
+const pageLoad = () =>{
+    location.reload()
+    // location.replace(document.referrer);
+}
