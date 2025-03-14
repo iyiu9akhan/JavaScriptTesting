d48ecf9773b5bcb90e65643033a950324365c397
iyiu9akhan <iyiu9akhan@gmail.com>
2025-03-14 22:29:56 +0600

    bmi calculator folder added || design portion || calculate page

---
+++ bmi calculator/js/script.js
@@ -0,0 +1,9 @@
+const calculate = () => {
+  const name = document.getElementById("name").value;
+  const gender = document.getElementById("gender").value;
+  const age = document.getElementById("age").value;
+  const height = document.getElementById("height").value;
+  const weight = document.getElementById("weight").value;
+  console.log(name,gender,age,height,weight);
+  
+};
