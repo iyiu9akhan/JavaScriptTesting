7642029c79a705925fa2e61701ed8a56f94f0922
iyiu9akhan <iyiu9akhan@gmail.com>
2025-03-15 02:03:03 +0600

    calculate portion || validation problem solved

--- bmi calculator/js/script.js
+++ bmi calculator/js/script.js
@@ -1,9 +1,14 @@
-// let error_sign = document.querySelector(".error_sign");
-let error_sign1 = document.querySelector(".error_sign1");
-let error_sign2 = document.querySelector(".error_sign2");
-let error_sign3 = document.querySelector(".error_sign3");
-let error_sign4 = document.querySelector(".error_sign4");
-let error_sign5 = document.querySelector(".error_sign5");
+let error_sign0 = document.getElementsByClassName("error_sign")[0];
+let error_sign1 = document.getElementsByClassName("error_sign")[1];
+let error_sign2 = document.getElementsByClassName("error_sign")[2];
+let error_sign3 = document.getElementsByClassName("error_sign")[3];
+let error_sign4 = document.getElementsByClassName("error_sign")[4];
+
+// let error_sign1 = document.querySelector(".error_sign1");
+// let error_sign2 = document.querySelector(".error_sign2");
+// let error_sign3 = document.querySelector(".error_sign3");
+// let error_sign4 = document.querySelector(".error_sign4");
+// let error_sign5 = document.querySelector(".error_sign5");
 
 
 let error_text = document.querySelector(".error_text");
@@ -15,51 +20,61 @@ const calculate = () => {
   const weight = document.getElementById("weight").value;
 
   if (!isNaN(name)) {
-    error_sign1.textContent = "!";
-    error_sign2.textContent = "";
-    error_sign3.textContent = "";
-    error_sign4.textContent = "";
-    error_sign5.textContent = "";
-    // error_sign.textContent = "1";
+    // error_sign1.textContent = "!";
+    // error_sign2.textContent = "";
+    // error_sign3.textContent = "";
+    // error_sign4.textContent = "";
+    // error_sign5.textContent = "";
+
+    error_sign0.innerHTML = "1";
     error_text.textContent = "missing name";
   } else if (!isNaN(gender)) {
-    error_sign1.textContent = "";
-    error_sign2.textContent = "!";
-    error_sign3.textContent = "";
-    error_sign4.textContent = "";
-    error_sign5.textContent = "";
-    // error_sign.textContent = "2";
+    // error_sign1.textContent = "";
+    // error_sign2.textContent = "!";
+    // error_sign3.textContent = "";
+    // error_sign4.textContent = "";
+    // error_sign5.textContent = "";
+    error_sign0.innerHTML = "";
+
+    error_sign1.innerHTML = "2";
     error_text.textContent = "missing gender";
   } else if (!isNaN(age)) {
-    error_sign1.textContent = "";
-    error_sign2.textContent = "";
-    error_sign3.textContent = "!";
-    error_sign4.textContent = "";
-    error_sign5.textContent = "";
-    // error_sign.textContent = "3";
+    // error_sign1.textContent = "";
+    // error_sign2.textContent = "";
+    // error_sign3.textContent = "!";
+    // error_sign4.textContent = "";
+    // error_sign5.textContent = "";
+    error_sign1.innerHTML = "";
+    error_sign2.innerHTML = "3";
     error_text.textContent = "missing age";
   } else if (!isNaN(height)) {
-    error_sign1.textContent = "";
-    error_sign2.textContent = "";
-    error_sign3.textContent = "";
-    error_sign4.textContent = "!";
-    error_sign5.textContent = "";
-    // error_sign.textContent = "4";
+    // error_sign1.textContent = "";
+    // error_sign2.textContent = "";
+    // error_sign3.textContent = "";
+    // error_sign4.textContent = "!";
+    // error_sign5.textContent = "";
+    error_sign2.innerHTML = "";
+
+    error_sign3.innerHTML = "4";
     error_text.textContent = "missing height";
   } else if (!isNaN(weight)) {
-    error_sign1.textContent = "";
-    error_sign2.textContent = "";
-    error_sign3.textContent = "";
-    error_sign4.textContent = "";
-    error_sign5.textContent = "!";
-    // error_sign.textContent = "5";
+    // error_sign1.textContent = "";
+    // error_sign2.textContent = "";
+    // error_sign3.textContent = "";
+    // error_sign4.textContent = "";
+    // error_sign5.textContent = "!";
+    error_sign3.innerHTML = "";
+
+    error_sign4.innerHTML = "5";
     error_text.textContent = "missing weight";
   } else {
-    error_sign1.textContent = "";
-    error_sign2.textContent = "";
-    error_sign3.textContent = "";
-    error_sign4.textContent = "";
-    error_sign5.textContent = "";
+    // error_sign1.textContent = "";
+    // error_sign2.textContent = "";
+    // error_sign3.textContent = "";
+    // error_sign4.textContent = "";
+    // error_sign5.textContent = "";
+    error_sign4.innerHTML = "";
+
     error_text.textContent = "done";
   }
 };
