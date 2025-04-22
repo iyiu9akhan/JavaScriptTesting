5176feaa0b7e990f8f92751e2d775f8d945524a7
iyiu9akhan <iyiu9akhan@gmail.com>
2025-04-22 21:12:29 +0600

    windows: callback/promise test

---
+++ call_back & promise/script.js
@@ -0,0 +1,20 @@
+// async way
+console.log("1920");
+console.log("1930");
+
+setTimeout(() => {
+  console.log("1940");
+}, 2000);
+
+sum(2,3) //sum func call test 
+
+console.log("1950");
+// async way
+
+ 
+function sum(a, b) {
+  var result = a + b;
+  console.log(result);
+}
+
+sum(1, 2);
