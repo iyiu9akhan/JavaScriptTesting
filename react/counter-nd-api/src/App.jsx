94735086ed5b653aa23952e0b83b12948874fcb4
iyiu9akhan <iyiu9akhan@gmail.com>
2025-05-06 23:49:02 +0600

    windows: api

--- react/counter-nd-api/src/App.jsx
+++ react/counter-nd-api/src/App.jsx
@@ -1,25 +1,35 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function counter(){
-  const [counter, setCounter] = useState(0);
-  const increment = ()=>{
-    if(counter<10){
-      setCounter(counter+1)
-    }
-  }
-  const decrement =()=>{
-    if(counter>0){
-      setCounter(counter-1)
-    }
-  }
-  return(
-    <>
-    <h1>counter: {counter}</h1>
-    <button onClick={increment}>increment</button>
-    <button onClick={decrement}>decrement</button>
-    </>
-  )
+// function counter() {
+//   const [counter, setCounter] = useState(0);
+//   const increment = () => {
+//     if (counter < 10) {
+//       setCounter(counter + 1);
+//     }
+//   };
+//   const decrement = () => {
+//     if (counter > 0) {
+//       setCounter(counter - 1);
+//     }
+//   };
+//   return (
+//     <>
+//       <h1>counter: {counter}</h1>
+//       <button onClick={increment}>increment</button>
+//       <button onClick={decrement}>decrement</button>
+//     </>
+//   );
+// }
+
+function info() {
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((active) => active.json())
+    .then((info)=>console.log(info, "info")
+    )
+  }, []);
 }
 
-export default counter
+// export default counter;
+export default info;
